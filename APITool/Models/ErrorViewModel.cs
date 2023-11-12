44ca2096f2548aa17ce6e3b6bc2c819a4905ed8c
Keith Petrone <keithpetrone@outlook.com>
2023-11-12 10:52:10 -0700

    Initial commit.
    Created .NET Core project in Visual Studio.

---
+++ APITool/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace APITool.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
